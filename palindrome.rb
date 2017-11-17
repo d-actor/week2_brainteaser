fc23ef6e87e05a1b4ccd7207fd611117eb04e452
Dan Actor <danltactor@gmail.com>
2017-11-17 10:15:37 -0700

    time up!

---
+++ palindrome.rb
@@ -0,0 +1,28 @@
+require 'colorize'
+require 'pry'
+
+@original = []
+@reverse = []
+
+def menu
+  print `clear`
+  puts "----PALINDROME CHECKER----".green
+  puts "I can check to see if a word is a palindrome!".green
+  puts "Enter the word you'd like to check: "
+  @word = STDIN.gets.strip.downcase
+  @original << @word.split("")
+  puts "You entered #{@word}.".yellow
+  check
+end
+
+def check
+  @original.each do |letter|
+    @reverse << @original[:&]
+  end
+
+binding.pry
+
+
+end
+
+menu
